330aec326698a2bc0ed68a578bc22a38512e75fb
marikatattoo <info.marikatattoo@gmail.com>
2025-05-19 23:09:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
-// Welcome message
-console.log("Welcome to Marika Tattoo.");
-
 // Fade in sections on scroll
 const sections = document.querySelectorAll('.section');
 
-const observer = new IntersectionObserver((entries, obs) => {
+const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('fade-in');
-      obs.unobserve(entry.target); // Stop observing once faded in
-    }
-  });
-}, { threshold: 0.1 });
-
-sections.forEach(section => {
-  observer.observe(section);
-});
+    if (entry.is
